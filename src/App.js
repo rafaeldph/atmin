076fe91be4d79f6031d28ca5c6f5c06089b3851b
Rafael Perez <prafael16@gmail.com>
2020-11-28 13:07:20 -0400

    Form and handle submit

--- src/App.js
+++ src/App.js
@@ -1,57 +1,24 @@
 import React from 'react';
+import Form from './Form';
 import './App.css';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
 
-    this.time_format = "**:**:**";
-    this.time_mask = "HH:mm:ss";
-    this.state = { lambda: this.time_mask, mu: this.time_mask, limit: "" };
+    this.state = { started: false, data: {} };
   }
 
-  doFormat(x, pattern, mask = "") {
-    let chars = x.replace(/[^0-9]/g, "").split('');
-    let count = 0;
-  
-    let formatted = '';
-    for (let i=0; i<pattern.length; i++) {
-      if (chars[count]) {
-        formatted += (/\*/.test(pattern[i])) ? chars[count++] : pattern[i];
-      } else if (mask && mask[i]) {
-        formatted += mask[i];
-      }
-    }
-
-    return formatted;
-  }
-
-  handleDateChange(name, target) {
-    const val = this.doFormat(target.value, this.time_format);
-    this.setState({ [name]: this.doFormat(target.value, this.time_format, this.time_mask) }, () => {
-      target.selectionStart = target.selectionEnd = val.length
-    });
-  }
-
-  handleLimitChange(value) {
-    this.setState({ limit: value.replace(/[^0-9]/g, "") })
+  handleStarting(values) {
+    console.log(values);
   }
 
   render() {
+    let Component = (this.state.started) ? Form : Form;
+
     return (
-      <div className="initial-form">
-        <div className="form-element">
-          <label htmlFor="lambda">Tiempo medio de llegada</label>
-          <input type="text" className="form-input" value={this.state.lambda} onChange={event => this.handleDateChange("lambda", event.target)} />
-        </div>
-        <div className="form-element">
-          <label htmlFor="mu">Tiempo medio de servicio</label>
-          <input type="text" className="form-input" value={this.state.mu} onChange={event => this.handleDateChange("mu", event.target)} />
-        </div>
-        <div className="form-element">
-          <label htmlFor="limit">Límite de la cola</label>
-          <input type="text" className="form-input" value={this.state.limit} onChange={event => this.handleLimitChange(event.target.value)} />
-        </div>
+      <div className="main-container">
+        <Component onSubmit={values => this.handleStarting(values)} />
       </div>
     );
   }
---
+++ src/Form.js
@@ -0,0 +1,95 @@
+import React from 'react';
+
+export default class Form extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.time_format = "**:**:**";
+    this.time_mask = "--:--:--";
+    this.time_regexp = /\d{2}:\d{2}:\d{2}/;
+
+    this.state = { lambda: this.time_mask, mu: this.time_mask, limit: "", errors: {} };
+  }
+
+  format(x, pattern, mask = "") {
+    let chars = x.replace(/[^0-9]/g, "").split('');
+    let count = 0;
+  
+    let formatted = '';
+    for (let i=0; i<pattern.length; i++) {
+      if (chars[count]) {
+        formatted += (/\*/.test(pattern[i])) ? chars[count++] : pattern[i];
+      } else if (mask && mask[i]) {
+        formatted += mask[i];
+      }
+    }
+
+    return formatted;
+  }
+
+  handleDateChange(name, target) {
+    const val = this.format(target.value, this.time_format);
+    this.setState({ [name]: this.format(target.value, this.time_format, this.time_mask) }, () => {
+      target.selectionStart = target.selectionEnd = val.length
+    });
+  }
+
+  handleLimitChange(value) {
+    this.setState({ limit: value.replace(/[^0-9]/g, "") })
+  }
+
+  handleSubmit() {
+    const error = "No está en el formato adecuado.";
+
+    let errors = ["lambda", "mu"].reduce((errors, index) => {
+      if (!this.time_regexp.test(this.state[index])) {
+        errors[index] = error;
+      }
+      return errors;
+    }, {});
+
+    if (!/^\d*$/g.test(this.state.limit)) {
+      errors['limit'] = error;
+    }
+
+    this.setState({ errors: errors });
+
+    if (!Object.keys(errors).length) {
+      let values = ["lambda", "mu"].reduce(
+        (values, index) => 
+        Object.assign(values, { 
+          [index]: this.state[index].split(':').reverse().reduce((result, current, index) => result + current * Math.pow(60,index), 0)
+        }), 
+        {}
+      );
+      if (this.state.limit) {
+        values['limit'] = parseInt(this.state.limit);
+      }
+      this.props.onSubmit(values);
+    }
+  }
+  
+  render() {
+    return (
+      <div className="initial-form">
+        <div className="form-element">
+          <label htmlFor="lambda">Tiempo medio de llegada (HH:mm:ss)</label>
+          <input type="text" className="form-input" value={this.state.lambda} onChange={event => this.handleDateChange("lambda", event.target)} />
+          {this.state.errors.lambda && <p className="form-error">{this.state.errors.lambda}</p>}
+        </div>
+        <div className="form-element">
+          <label htmlFor="mu">Tiempo medio de servicio (HH:mm:ss)</label>
+          <input type="text" className="form-input" value={this.state.mu} onChange={event => this.handleDateChange("mu", event.target)} />
+          {this.state.errors.mu && <p className="form-error">{this.state.errors.mu}</p>}
+        </div>
+        <div className="form-element">
+          <label htmlFor="limit">Límite de la cola</label>
+          <input type="text" className="form-input" value={this.state.limit} onChange={event => this.handleLimitChange(event.target.value)} />
+        </div>
+        <div className="form-element">
+          <button className="form-submit-button" onClick={this.handleSubmit.bind(this)}>Continuar</button>
+        </div>
+      </div>
+    );
+  }
+};
